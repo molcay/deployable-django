9536a4cb8cda88a61c9ccb5c39ec0b98e3002dfc
M. Olcay TERCANLI <molcay@mail.com>
2021-03-07 22:45:20 +0300

    Fix static files

--- project/settings.py
+++ project/settings.py
@@ -126,11 +126,6 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    str(BASE_DIR / 'static'),
-)
-
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
